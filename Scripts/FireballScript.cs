91d091ada3d2dc67554893b8080be3ea0f58b4c7
swishypants <shamuflute@gmail.com>
2016-04-10 05:48:20 -0400

    explosions

---
+++ Scripts/FireballScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireballScript : MonoBehaviour {
+
+	public GameObject explosionPrefab;
+
+	void OnCollisionEnter(Collision col) {
+		//Debug.Log (col.transform.name);
+		ContactPoint contact = col.contacts[0];
+		Quaternion rot = Quaternion.FromToRotation(Vector3.up, contact.normal);
+		Vector3 pos = gameObject.transform.position;
+		Instantiate(explosionPrefab, pos, rot);
+		Destroy(gameObject);
+	}
+}
